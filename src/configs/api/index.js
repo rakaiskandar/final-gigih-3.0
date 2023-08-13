b7512d74da95fae54c83060bd0e57db46d3da11a
rakaiskandar <iskandarraka4@gmail.com>
2023-08-13 20:58:31 +0700

    chore: configs api endpoint

---
+++ src/configs/api/index.js
@@ -0,0 +1,11 @@
+export const api = {
+    BASE_URL: "http://localhost:8000/api",
+    GET_VIDEOS: '/videos',
+    GET_VIDEOS_ID: '/videos/',
+    SEARCH_VIDEOS: '/videos/search',
+    GET_PRODUCTS: '/products/',
+    GET_COMMENTS: '/comments/',
+    POST_COMMENTS: '/comments/'
+}
+
+
